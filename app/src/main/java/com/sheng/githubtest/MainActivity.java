95cc553bcad16be0af45e1a23538a4955fe86de7
Llewellin <exsequens15@gmail.com>
2015-07-14 15:08:32 +0800

    hip...

--- app/src/main/java/com/sheng/githubtest/MainActivity.java
+++ app/src/main/java/com/sheng/githubtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.sheng.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("dsd", "sds");
         setContentView(R.layout.activity_main);
     }
 
